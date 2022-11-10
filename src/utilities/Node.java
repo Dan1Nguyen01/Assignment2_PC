b8b7d236a755685bdb60b394e55a85ffa60c1007
Dan1Nguyen01 <98774378+Dan1Nguyen01@users.noreply.github.com>
2022-11-09 18:03:39 -0700

    Updated Node
    UPDATED NODE

--- src/utilities/Node.java
+++ src/utilities/Node.java
@@ -13,13 +13,11 @@ public class Node implements Serializable {
 	private Node next;
 	private Node prev;
 	
-	public Node () {
-		
-	}
-	public Node(Object data, Node next, Node prev) {
+	
+	public Node(Object data) {
 		this.data = data;
-		this.next = next;
-		this.prev = prev;
+		this.next = null;
+		this.prev = null;
 	}
 
 	public Object getData() {
